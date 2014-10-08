d05446c644bf6594f2cf51ac7e552278028f82b4
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-10-08 12:25:20 +0200

    BeaconManager neu aufgesetzt mit Module Pattern

--- js/beacon_manager.js
+++ js/beacon_manager.js
@@ -1,17 +1,29 @@
-console.log( '--- BeaconManager ---' );
+console.log( '--- Start BeaconManager Installation ---' );
 
-window.BeaconManager = {
-  updateBeacons: function ( beaconArray ) {
-    console.log( "--> updateBeacons: " + beaconArray.count );
+window.BeaconManager = ( function() {
+    var beaconArray = []; //private
     
-    $beaconListe = $( '.beaconliste' );
-    $beaconListe.html('');
-    
-    beaconArray.forEach( function( beacon ) {
-      console.log( 'beacon:' + beacon );
-      $beaconListe.append( $( '<li class="beacon"><span class="ident">' + beacon.major + '.Stock / Beacon ' + beacon.minor + '</span><span class="signalstaerke">' + beacon.rssi + '</span><div class="clear"></div></li>' ) );
-    } );
-  }
-}
+    return { //exposed to public
+      
+      updateBeacons: function( inBeaconArray ) {
+        beaconArray = inBeaconArray;
+        
+        console.log( "--> updateBeacons: " + beaconArray.count );
+        
+        $beaconListe = $( '.beaconliste' );
+        $beaconListe.html('');
+        
+        beaconArray.forEach( function( beacon ) {
+          console.log( 'beacon:' + beacon );
+          $beaconListe.append( $( '<li class="beacon"><span class="ident">' + beacon.major + '.Stock / Beacon ' + beacon.minor + '</span><span class="signalstaerke">' + beacon.rssi + '</span><div class="clear"></div></li>' ) );
+        } );
+      },
+      
+      getBeaconArray: function() {
+        return beaconArray;
+      }
+    }
+}());
 
-console.log( 'ok? ' + window.BeaconManager );
+console.log( 'ready?:' + ( window.BeaconManager !== undefined ) );
+console.log( '--- End BeaconManager Installation ---' );
