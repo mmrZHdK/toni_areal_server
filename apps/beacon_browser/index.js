e9db2817e20b9e9be2b1de142efba90c49e93494
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-10-08 12:47:12 +0200

    Fehlerbehebung im BeaconManager, EventListener in BeaconBrowser

--- apps/beacon_browser/index.js
+++ apps/beacon_browser/index.js
@@ -1,2 +1,20 @@
 console.log( '=== Beacon Browser ===' );
 
+$( document ).ready( function() {
+  console.log( '-- Document Ready' );
+  
+  window.BeaconManager.addEventListener( 'update', function( inBeacons ) {
+    
+    console.log( "--> update Event für Beacons: " + inBeacons.count );
+    
+    var $beaconListe = $( '.beaconliste' );
+    $beaconListe.html( '' );
+    
+    inBeacons.forEach( function( beacon ) {
+      console.log( 'beacon:' + beacon );
+      $beaconListe.append( $( '<li class="beacon"><span class="ident">' + beacon.major + '.Stock / Beacon ' + beacon.minor + '</span><span class="signalstaerke">' + beacon.rssi + '</span><div class="clear"></div></li>' ) );
+    } );
+    
+  })
+  
+} );
--- js/beacon_manager.js
+++ js/beacon_manager.js
@@ -2,25 +2,27 @@ console.log( '--- Start BeaconManager Installation ---' );
 
 window.BeaconManager = ( function() {
     var beaconArray = []; //private
+    var eventListeners = [];
     
     return { //exposed to public
       
       updateBeacons: function( inBeaconArray ) {
         beaconArray = inBeaconArray;
         
-        console.log( "--> updateBeacons: " + beaconArray.count );
-        
-        $beaconListe = $( '.beaconliste' );
-        $beaconListe.html('');
-        
-        beaconArray.forEach( function( beacon ) {
-          console.log( 'beacon:' + beacon );
-          $beaconListe.append( $( '<li class="beacon"><span class="ident">' + beacon.major + '.Stock / Beacon ' + beacon.minor + '</span><span class="signalstaerke">' + beacon.rssi + '</span><div class="clear"></div></li>' ) );
-        } );
+        // informiere Listeners
+        eventListeners.forEach( function( inListener ) {
+          if ( inListener[ 0 ] === 'update' ) {
+            inListener[ 1 ].call( null, beaconArray );
+          }
+        });
       },
       
       getBeaconArray: function() {
         return beaconArray;
+      },
+      
+      addEventListener: function ( inEvent, inCallback ) {
+        eventListeners.push( [ inEvent, inCallback ] );
       }
     }
 }());
