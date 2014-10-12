a98c6e2c395d9f0e8ba80bfafee66bf651c2f239
Olivia <olivia.stadler@zhdk.ch>
2014-10-12 21:51:03 +0200

    Lunchapp Olivia

---
+++ apps/toni_lunch/js/main.js
@@ -0,0 +1,33 @@
+
+
+
+$(document).ready(function(){
+
+  $('#idMenu').click(function() {	
+    $('.info').toggleClass("info_out_top");
+    $('.swiper-container').toggleClass("swiper-container_out_bottom");
+    $('.menubar').toggleClass("menubar_out_bottom");
+  	
+    $('.info').toggleClass("info-in");
+    $('.swiper-container').toggleClass("swiper-container-in");
+    $('.menubar').toggleClass("menubar-in");
+
+  	});
+
+  $('.info').click(function() {	
+    $('.info').toggleClass("info_out_top");
+    $('.swiper-container').toggleClass("swiper-container_out_bottom");
+    $('.menubar').toggleClass("menubar_out_bottom");
+  	
+    $('.info').toggleClass("info-in");
+    $('.swiper-container').toggleClass("swiper-container-in");
+    $('.menubar').toggleClass("menubar-in");
+  	});
+
+  
+   
+
+});
+
+
+
