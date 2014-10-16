0cda545c0021a240ea8b8292237331137ffddc54
Olivia <olivia.stadler@zhdk.ch>
2014-10-16 23:05:50 +0200

    bereinigter Code

--- apps/toni_lunch/js/detail.js
+++ apps/toni_lunch/js/detail.js
@@ -1,28 +1,34 @@
 $(document).ready(function(){
-	$('#idMenu').click(function() {	
-    $('.info').toggleClass("info_out_top");
-    $('.menubar').toggleClass("menubar_out_bottom");
-  	
-    $('.info').toggleClass("info-in");
-    $('.menubar').toggleClass("menubar-in");
+	
 
-  	});
 
-  $('.info').click(function() {	
-    $('.info').toggleClass("info_out_top");
-    $('.menubar').toggleClass("menubar_out_bottom");
-  	
-    $('.info').toggleClass("info-in");
-    $('.menubar').toggleClass("menubar-in");
-  	});
+
+
+	
+
+	var d = new Date();
+
+	var month = d.getMonth()+1;
+	var day = d.getDate();
+
+	var outputBernoulli = ((''+day).length<2 ? '0' : '') + day + "." +
+	((''+month).length<2 ? '0' : '') + month + '.' +
+	d.getFullYear() ;
+
+
+
+// Get the date, move to Monday (if not already Monday),
+// then alert the date to the user in a different format.
+var montagWestend = Date.parse('monday').toString('d' + "." + "M" + ".");
+var freitagWestend = Date.parse('friday').toString('d' + "." + "M" + "." + "yyyy");
+ 
 
 
 
 	var pdfUrl4akt = "http://4akt.ch/uploads/pdf/tagesmenu/menu.pdf";
 	var pdfUrlLunch5 = "http://www.lunch-5.ch/menu/menuplan.pdf";
-	var pdfUrlWestend = "http://westend-zuerich.ch/uploads/PDF/13.10.-17.10.2014.pdf";
-	var jpgUrlBernoulli = "http://www.brasseriebernoulli.ch/mittagskarte/15.10.2014.jpg";
-
+	var pdfUrlWestend = "http://westend-zuerich.ch/uploads/PDF/" + montagWestend + "-" + freitagWestend + ".pdf";
+	var jpgUrlBernoulli = "http://www.brasseriebernoulli.ch/mittagskarte/" + outputBernoulli + ".jpg";
 
 	var mapId = getUrlParameter('mapId');
 	var iframesrc ="";
