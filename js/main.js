c5afe042a7f94dfd5d738ec7d3faa3fb4114b501
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-10-25 16:55:10 +0200

    Neue Startseite mit AppIcons und Erklärung

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+$( document ).ready( function() {
+  // Start des Programme, Dokument geladen
+  
+  var timerID = window.setTimeout( function() {
+    $( '#about_text' ).hide( 500 );
+  }, 3000 );
+  
+  $( '#about' ).on( 'click', function() {
+    // Klick auf die ABOUT Taste
+    $( '#about_text' ).toggle( 300 );
+  } );
+  
+/*
+  $( '#info' ).on( 'click', function() {
+    // Klick auf die INFO Taste
+  } );
+*/
+  
+} );
