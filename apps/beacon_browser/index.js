41de5064bf47f1896fd6d919b9c32c84de021959
Magnus Rembold <magnus.rembold@zhdk.ch>
2014-10-25 17:58:05 +0200

    Beacon Browser checkt ob er Beacons verarbeiten kann

--- apps/beacon_browser/index.js
+++ apps/beacon_browser/index.js
@@ -3,22 +3,32 @@ console.log( '=== Beacon Browser ===' );
 $( document ).ready( function() {
   console.log( '-- Document Ready' );
   
+  var bbBeacons = [];
+  var $beaconListe = $( '.beaconliste' );
+  
+  var timerID = window.setTimeout( function() {
+    $( '#toll' ).hide();
+    $( '#mist' ).show();
+  }, 8000 );
+  
   window.BeaconManager.addEventListener( 'update', function( inBeacons ) {
     
     console.log( "--> update Event für Beacons: " + inBeacons.count );
     
-    var $beaconListe = $( '.beaconliste' );
     $beaconListe.html( '' );
     
+    inBeacons.forEach( function( beacon ) {
+      
+    } );
     inBeacons.forEach( function( beacon ) {
       console.log( 'beacon:' + beacon );
       $beaconListe.append( $( '<li class="beacon"><div class="ident">' + beacon.major + '.Stock / Beacon ' + beacon.minor + '</div><div class="signalstaerke">' + beacon.rssi + '</div><div class="balken ' + beacon.proximity + '" style="width:' + ( 100 + beacon.rssi ) + '%">&nbsp;</div></li>' ) );
     } );
     
+    if ( timerID !== null ) {
+      window.clearTimeout( timerID );
+      timerID = null;
+    }
   })
   
-  window.BeaconManager.updateBeacons( [
-        { major:3, minor:12, proximity:'far', rssi:-72 },
-        { major:3, minor:14, proximity:'near', rssi:-61 }
-  ] );
 } );
